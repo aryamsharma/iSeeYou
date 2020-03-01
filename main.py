f3074add2d53c1bf8dc46d8f90e25349bf33b443
Aryam Sharma <aryamsharma@X-2.local>
2020-02-29 22:51:00 -0500

    Added automation

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import i_see_you
-import cropper
-from text_animator import Animation
 import os
+from time import sleep
 
-print("Starting software")
+import schedule
 
-if bool([i for i in os.listdir("data/tocrop") if not i.startswith(".")]):
-    should_crop = True if input("There are images in the to_crop file, would you like to add new students? (y/n)").lower() == "y" else False
+import cropper
+import i_see_you
+from text_animator import Animation
 
-    if should_crop:
-        cropper.main(int(input("Class_no: ")))
-    
-else:
-    Animation.to_animate(text=Animation.standard_text, to_sleep=0.001)
 
-    print("Running main Script")
-    i_see_you.main()
+def job():
+    if bool([i for i in os.listdir("data/tocrop") if not i.startswith(".")]):
+        should_crop = True if input("There are images in the to_crop file, would you like to add new students? (y/n)").lower() == "y" else False
+
+        if should_crop:
+            cropper.main(int(input("Class_no: ")))
+        
+    else:
+        Animation.to_animate(text=Animation.standard_text, to_sleep=0.001)
+
+        print("Running main Script")
+        i_see_you.main()
+
+if __name__ == "__main__":
+    job()
+    """
+    schedule.every().day.at("08:15").do(job)
+
+    while True:
+        schedule.run_pending()
+        sleep(10)
+    """
